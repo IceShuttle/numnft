5ef77cb00b273093e3376d0877f88cf5b141d72a
IceShuttle <IceShuttle@Debian>
2024-02-01 21:28:32 +0530

    added_owner_check

--- lib.rs
+++ lib.rs
@@ -4,21 +4,23 @@
 pub mod numnft {
 
     use ink::prelude::string::String;
+    // use ink_e2e::env_logger::fmt::Timestamp;
 
     #[ink(storage)]
     pub struct NFT {
-        author: String,
-        owner: String,
+        owner_name: String,
+        owner: AccountId,
+        creationtime: Timestamp,
         value: u32,
     }
 
     impl NFT {
         #[ink[constructor]]
-        pub fn new(auth: String, number: u32) -> Self {
-            let own = auth.clone();
+        pub fn new(own_name: String, number: u32) -> Self {
             Self {
-                author: auth,
-                owner: own,
+                owner_name: own_name,
+                owner: Self::env().caller(),
+                creationtime: Self::env().block_timestamp(),
                 value: number,
             }
         }
@@ -29,20 +31,19 @@ pub mod numnft {
         }
 
         #[ink(message)]
-        pub fn get_author(&self) -> String {
-            let auth = self.author.clone();
-            auth
-        }
-
-        #[ink(message)]
-        pub fn get_owner(&self) -> String {
-            let own = self.owner.clone();
+        pub fn get_owner_name(&self) -> String {
+            let own = self.owner_name.clone();
             own
         }
 
         #[ink(message)]
-        pub fn sell(&mut self, own: String) {
-            self.owner = own;
+        pub fn sell(&mut self, new_owner: AccountId, new_owner_name: String) {
+            if Self::env().caller() == self.owner {
+                self.owner_name = new_owner_name;
+                self.owner = new_owner;
+            } else {
+                panic!("Error You are not the owner");
+            }
         }
     }
 }
